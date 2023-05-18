0f5fc17b94a851735222d7c42c7c967ba8bb1c02
Jonas Rosengren <rosengrenjonas98@gmail.com>
2023-05-18 22:34:01 +0200

    update prompt

--- utils/index.ts
+++ utils/index.ts
@@ -9,13 +9,14 @@ const TASK_DESCRIPTION = `
 Your task is to perform a detailed review of the provided text.
   In your feedback, please focus on these aspects:
 
-  1. **Grammar:** Correct any grammatical errors such as incorrect verb tenses, misplaced punctuation, sentence fragments, etc. 
-  2. **Typos:** Fix any spelling mistakes or typographical errors you find.
-  3. **Completeness of Information:** Assess whether the text provides a full understanding of the topic being discussed. Suggest where more details or explanations could be added.
-  4. **Word Choice:** Check if the chosen words, phrases, and expressions are clear, precise, and effective.
-  5. **Accuracy** Be critical, is the information accurate?
+  **Grammar:** Correct any grammatical errors such as incorrect verb tenses, misplaced punctuation, sentence fragments, etc. 
+  **Typos:** Fix any spelling mistakes or typographical errors you find.
+  **Exhaustiveness:** Assess whether the text provides a full understanding of the topic being discussed. Suggest where more details or explanations could be added.
+  **Word Choice:** Check if the chosen words, phrases, and expressions are clear, precise, and effective.
+  **Veracity** Be critical, is the information accurate? Could it be misleading?
+
   
-  Give feedback on this text. Give the feedack as a html ordered list.
+  Give the feedback as a html ordered list. Your focus should be on the analysis of the text.
       
   After the listed feedback, output the original text with your comments annotated. 
   
@@ -36,16 +37,16 @@ Colors:
 <ol>
     <li><strong>Grammar:</strong> Correct the grammatical errors - use "It's" at the start of sentences, place a comma after "However," and use "it's" instead of "its" when it means "it is."</li>
     <li><strong>Typos:</strong> Fix the spelling mistakes - "lazzy" should be "lazy," and "usefull" should be "useful."</li>
-    <li><strong>Completeness of Information:</strong> Elaborate on why the sentence demonstrates all alphabets, and include that pangrams are also used in typography and keyboarding practice.</li>
+    <li><strong>Exhaustiveness:</strong> Elaborate on why the sentence demonstrates all alphabets, and include that pangrams are also used in typography and keyboarding practice.</li>
     <li><strong>Word Choice:</strong> Replace "old sentence" with "classic sentence" or "well-known phrase," and consider rephrasing "not much usefull outside that" to "has limited practical use beyond this."</li>
-    <li><strong>Accuracy:</strong> Correct the assertion that the sentence has limited use beyond demonstrating all alphabets, pointing out its other uses in typography and keyboarding practice.</li>
+    <li><strong>Veracity:</strong> Correct the assertion that the sentence has limited use beyond demonstrating all alphabets, pointing out its other uses in typography and keyboarding practice.</li>
 </ol>
   
   and the annotated original text should follow this pattern: 
     <p> other_part <span style="background-color: color title="error_type. comment."> commented_part</span> other_part </p>
   
   so as a concrete example:
-    <p> However, its <span style="background-color: #cc5500" title="Accuracy: Point out other uses of the sentence in typography and keyboarding practice.">not much usefull</span><span style="background-color: #800080" title="Word Choice: Consider rephrasing 'its not much useful outside that' to 'has limited practical use beyond this.'"> outside that.</span></p>
+    <p> However, its <span style="background-color: #cc5500" title="Veracity: Point out other uses of the sentence in typography and keyboarding practice.">not much usefull</span><span style="background-color: #800080" title="Word Choice: Consider rephrasing 'its not much useful outside that' to 'has limited practical use beyond this.'"> outside that.</span></p>
 
  Make sure to only span the part of the text that is of interest for the comment, and insert the appropriate comment in the title-attribute.
  Also, use <hr>-tags to separate the feedback list and annotated text.
