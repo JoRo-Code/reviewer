ce3e56a1b69486c8a8af1a61c9f869b30e637e64
Jonas Rosengren <rosengrenjonas98@gmail.com>
2023-05-18 19:57:31 +0200

    improve feedback

--- utils/index.ts
+++ utils/index.ts
@@ -5,13 +5,50 @@ import {
   ReconnectInterval,
 } from 'eventsource-parser';
 
-const INPUT_OUTPUT_EXAMPLE = `
-  If the original sentence is::
-    The quick brown fox jumps over the lazzy dog. It's an old sentence used for demonstrating all alphabets. However it's not much usefull outside that.
+const TASK_DESCRIPTION = `
+Your task is to perform a detailed review of the provided text.
+  In your feedback, please focus on these aspects:
 
-  Your HTML annotated sentence should look like: 
-    <p>The quick brown fox jumps over the <span style="background-color: #8B0000" title="Typographical error. The correct spelling is 'lazy'.">lazzy</span> dog. <span style="background-color: #800080" title="Ambiguous pronoun reference. Consider revising to 'This is an old sentence...'">It's</span> an old sentence used for demonstrating all <span style="background-color: #006400" title="Consider revising for clarity. Perhaps 'all the letters of the alphabet' would work better.">alphabets</span>. However <span style="background-color: #00008B" title="Incorrect form. Use 'it's' instead of 'it' to mean 'it is'.">it's</span> not much <span style="background-color: #8B0000" title="Incorrect word usage. Use 'useful' instead of 'usefull'.">usefull</span> outside that.</p>
+  1. **Grammar:** Correct any grammatical errors such as incorrect verb tenses, misplaced punctuation, sentence fragments, etc. 
+  2. **Typos:** Fix any spelling mistakes or typographical errors you find.
+  3. **Completeness of Information:** Assess whether the text provides a full understanding of the topic being discussed. Suggest where more details or explanations could be added.
+  4. **Word Choice:** Check if the chosen words, phrases, and expressions are clear, precise, and effective.
+  5. **Accuracy** Be critical, is the information accurate?
+  
+  Give feedback on this text. Give the feedack as a html ordered list.
+      
+  After the listed feedback, output the original text with your comments annotated. 
+  
+  Make sure to not change the original text, only add your annotated comments.
+  Also, use a <hr>-tag to separate the the feedback list and the annotated text. 
 
+Colors:
+    Dark Blue (#00008B) is used for incorrect form or grammar.
+    Dark Red (#8B0000) is used for typographical errors.
+    Purple (#800080) is used for ambiguous or unclear phrases.
+    Dark Green (#006400) is used for places where additional clarity could be beneficial.
+    Dark Orange (#cc5500) is used for questionable statements.
+
+  If the original text is:
+    The quick brown fox jumps over the lazzy dog. It's an old sentence used for demonstrating all alphabets. However its not much usefull outside that.
+  
+  The feedback list should look like this:
+<ol>
+    <li><strong>Grammar:</strong> Correct the grammatical errors - use "It's" at the start of sentences, place a comma after "However," and use "it's" instead of "its" when it means "it is."</li>
+    <li><strong>Typos:</strong> Fix the spelling mistakes - "lazzy" should be "lazy," and "usefull" should be "useful."</li>
+    <li><strong>Completeness of Information:</strong> Elaborate on why the sentence demonstrates all alphabets, and include that pangrams are also used in typography and keyboarding practice.</li>
+    <li><strong>Word Choice:</strong> Replace "old sentence" with "classic sentence" or "well-known phrase," and consider rephrasing "not much usefull outside that" to "has limited practical use beyond this."</li>
+    <li><strong>Accuracy:</strong> Correct the assertion that the sentence has limited use beyond demonstrating all alphabets, pointing out its other uses in typography and keyboarding practice.</li>
+</ol>
+  
+  and the annotated original text should follow this pattern: 
+    <p> other_part <span style="background-color: color title="error_type. comment."> commented_part</span> other_part </p>
+  
+  so as a concrete example:
+    <p> However, its <span style="background-color: #cc5500" title="Accuracy: Point out other uses of the sentence in typography and keyboarding practice.">not much usefull</span><span style="background-color: #800080" title="Word Choice: Consider rephrasing 'its not much useful outside that' to 'has limited practical use beyond this.'"> outside that.</span></p>
+
+ Make sure to only span the part of the text that is of interest for the comment, and insert the appropriate comment in the title-attribute.
+ Also, use <hr>-tags to separate the feedback list and annotated text.
 `
 
 const createPrompt = (
@@ -19,28 +56,8 @@ const createPrompt = (
   outputLanguage: string,
   inputCode: string,
 ) => endent`
-As an advanced AI language model, your task is to perform a detailed review of the provided text. Your feedback should be interwoven directly into the original text using HTML '<span>' tags for annotations. Your comments should appear when hovering over the highlighted sections. Here's how to do it: wrap the section you are referring to in '<span style="background-color: color" title="Your comment here">highlighted text</span>'.
 
-Remember: Do NOT use <!-- --> comments, as they will not be visible when hovering over the text. 
-
-In your review, please focus on these aspects:
-
-1. **Grammar:** Correct any grammatical errors such as incorrect verb tenses, misplaced punctuation, sentence fragments, etc. 
-2. **Typos:** Fix any spelling mistakes or typographical errors you find.
-3. **Completeness of Information:** Assess whether the text provides a full understanding of the topic being discussed. Suggest where more details or explanations could be added.
-4. **Word Choice:** Check if the chosen words, phrases, and expressions are clear, precise, and effective.
-
-Colors:
-    Dark Blue (#00008B) is used for incorrect form or grammar.
-    Dark Red (#8B0000) is used for typographical errors.
-    Purple (#800080) is used for ambiguous or unclear phrases.
-    Dark Green (#006400) is used for places where additional clarity could be beneficial.
-
-Here's a clear example of what you should do:
-  
-  ${INPUT_OUTPUT_EXAMPLE}
-
-  Your goal is to help improve the overall quality and clarity of the text, but don't change anything. Only comment on the input text.
+  ${TASK_DESCRIPTION}
 
   Input text:
   ${inputCode}
