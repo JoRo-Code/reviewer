8f12fb6c6ba6f95c2cde7fd3604d4fd93d4ed324
Jonas Rosengren <rosengrenjonas98@gmail.com>
2023-05-19 12:13:42 +0200

    fix mobile popup dialog

--- components/InfoButton.tsx
+++ components/InfoButton.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
-import Modal from 'react-modal';
 import { HiOutlineInformationCircle } from "react-icons/hi2";
+import React from 'react';
+import { isMobile } from 'react-device-detect';
+
 
 import {
   AlertDialog,
@@ -14,16 +15,37 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 
+export const InfoButton: React.FC = () => {
 
-export const InfoButton = () => {
+
   const description = `
     Here you'll get feedback on your text.
     
     The feedback will be provided both as a list of comments and highlighted areas of the original text, with different colors depending on the type of comment.
+    Hover on the highlights to get additional comments (not supported for mobile).
 
     Hit the button to start!
   
   `
+  {
+    return (
+    isMobile ?
+    <button>
+        <HiOutlineInformationCircle className="text-2xl transform hover:scale-125 transition-all duration-200" onClick={() => alert(description)}/>
+    </button>
+      : (
+        <LaptopInfoButton description={description} />
+      ))
+  }
+}
+
+
+
+interface LaptopInfoButtonProps {
+  description: string;
+}
+
+const LaptopInfoButton: React.FC<LaptopInfoButtonProps> = ({ description }) => {
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
@@ -45,4 +67,7 @@ export const InfoButton = () => {
     </AlertDialog>
   )
 }
+
+export default LaptopInfoButton;
+
 
